9739732368d4b9a05cfd82f174077e0f7eb6a2fa
Ryan Phillips <ryan.phillips@rackspace.com>
2014-04-15 10:27:26 -0500

    add agentHostInfo

--- agent.go
+++ agent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/codegangsta/cli"
@@ -14,10 +15,38 @@ func agentTokenList(c *cli.Context) {
 	Display(tokens)
 }
 
+func agentHostInfo(c *cli.Context) {
+	agId := c.String("agent-id")
+	if len(agId) == 0 {
+		fmt.Println("Agent ID Missing")
+		return
+	}
+	agentType := c.String("type")
+	if len(agentType) == 0 {
+		fmt.Println("Type Missing")
+		return
+	}
+
+	info, err := GetClient().AgentHostInfo(agId, agentType)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Display(info)
+}
+
 var AgentsExports []cli.Command = []cli.Command{
 	{
 		Name:   "agent_tokens.list",
 		Usage:  "Agent Token List",
 		Action: agentTokenList,
+	},
+	{
+		Name:   "agent_host.info",
+		Usage:  "Agent Host Info",
+		Action: agentHostInfo,
+		Flags: []cli.Flag{
+			cli.StringFlag{"agent-id", "", "The Agent ID"},
+			cli.StringFlag{"type", "", "Host Info Type"},
+		},
 	},
 }
