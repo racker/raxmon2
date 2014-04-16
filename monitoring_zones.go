7e1b7d765a44ce0c249dd71188eae0f314366885
Ryan Phillips <ryan.phillips@rackspace.com>
2014-04-15 19:25:18 -0500

    add monitoringZone traceroute

--- monitoring_zones.go
+++ monitoring_zones.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/codegangsta/cli"
@@ -15,10 +16,41 @@ func monitoringZoneList(c *cli.Context) {
 	Display(zones)
 }
 
+func monitoringZoneTraceroute(c *cli.Context) {
+	id := c.String("id")
+	if len(id) == 0 {
+		fmt.Println("Monitoring Zone ID Missing")
+		return
+	}
+	target := c.String("target")
+	if len(target) == 0 {
+		fmt.Println("Target Missing")
+		return
+	}
+	resolver := c.String("target_resolver")
+
+	route, err := GetClient().TracerouteMonitoringZone(id, target, resolver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Display(route)
+}
+
 var MonitoringZonesExports []cli.Command = []cli.Command{
 	{
 		Name:   "monitoring_zones.list",
 		Usage:  "Monitoring Zones List",
 		Action: monitoringZoneList,
+	},
+	{
+		Name:   "monitoring_zones.traceroute",
+		Usage:  "Monitoring Zones Traceroute",
+		Action: monitoringZoneTraceroute,
+		Flags: []cli.Flag{
+			cli.StringFlag{"id", "", "The Monitoring Zone ID"},
+			cli.StringFlag{"target", "", "target"},
+			cli.StringFlag{"target_resolver", "IPv4", "Target Resolver (Default: IPv4)"},
+		},
 	},
 }
