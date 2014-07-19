ea94877cf85ace5f4c7173503e67c2ff921df7b2
Simon Vetter <simon.vetter@runbox.com>
2014-07-19 15:33:27 -0700

    feat(checks): add update method

--- checks.go
+++ checks.go
@@ -76,6 +76,12 @@ func checkCreate(c *cli.Context) {
 		check.TargetResolver = &target_resolver
 	}
 
+	metadata_str := c.String("metadata")
+	if len(metadata_str) > 0 {
+		metadata_obj := StringToDict(metadata_str)
+		check.Metadata = &metadata_obj
+	}
+
 	check.Timeout = &timeout
 	check.Period = &period
 
@@ -144,6 +150,74 @@ func checkDisable(c *cli.Context) {
 	checkEnableDisable(c, true)
 }
 
+func checkUpdate(c *cli.Context) {
+	check := make(map[string]interface{})
+
+	enId := c.String("entity-id")
+	chId := c.String("id")
+
+	if len(enId) == 0 {
+		Die("Entity ID Missing")
+	}
+
+	if len(chId) == 0 {
+		Die("Check ID Missing")
+	}
+
+	label := c.String("label")
+	if len(label) > 0 {
+		check["label"] = &label
+	}
+
+	timeout := c.Int("timeout")
+	if timeout != -1 {
+		check["timeout"] = &timeout
+	}
+
+	period := c.Int("period")
+	if period != -1 {
+		check["period"] = &period
+	}
+
+	details_str := c.String("details")
+	if len(details_str) > 0 {
+		details_obj := StringToDict(details_str)
+		check["details"] = &details_obj
+	}
+
+	monitoring_zones_str := c.String("monitoring-zones")
+	if len(monitoring_zones_str) > 0 {
+		check["monitoring_zones_poll"] = StringToList(monitoring_zones_str)
+	}
+
+	target_alias := c.String("target-alias")
+	if len(target_alias) > 0 {
+		check["target_alias"] = &target_alias
+	}
+
+	target_hostname := c.String("target-hostname")
+	if len(target_hostname) > 0 {
+		check["target_hostname"] = &target_hostname
+	}
+
+	target_resolver := c.String("target-resolver")
+	if len(target_resolver) > 0 {
+		check["target_resolver"] = &target_resolver
+	}
+
+	metadata_str := c.String("metadata")
+	if len(metadata_str) > 0 {
+		metadata_obj := StringToDict(metadata_str)
+		check["metadata"] = &metadata_obj
+	}
+
+	err := GetClient().UpdateCheck(enId, chId, &check)
+	if err != nil {
+		Die(err)
+	}
+	fmt.Printf("%s updated\n", chId)
+}
+
 var ChecksExports []cli.Command = []cli.Command{
 	{
 		Name:   "checks.create",
@@ -158,6 +232,7 @@ var ChecksExports []cli.Command = []cli.Command{
 			cli.StringFlag{"target-alias", "", ""},
 			cli.StringFlag{"target-hostname", "", ""},
 			cli.StringFlag{"target-resolver", "", ""},
+			cli.StringFlag{"metadata", "", ""},
 			cli.IntFlag{"timeout", -1, ""},
 			cli.IntFlag{"period", -1, ""},
 		},
@@ -195,6 +270,24 @@ var ChecksExports []cli.Command = []cli.Command{
 		Flags: []cli.Flag{
 			cli.StringFlag{"entity-id", "", "The Entity ID"},
 			cli.StringFlag{"id", "", "The Check ID"},
+		},
+	},
+	{
+		Name:   "checks.update",
+		Usage:  "Check Update",
+		Action: checkUpdate,
+		Flags: []cli.Flag{
+			cli.StringFlag{"entity-id", "", "The Entity ID"},
+			cli.StringFlag{"id", "", "The Check ID"},
+			cli.StringFlag{"label", "", ""},
+			cli.StringFlag{"details", "", ""},
+			cli.StringFlag{"monitoring-zones", "", ""},
+			cli.StringFlag{"target-alias", "", ""},
+			cli.StringFlag{"target-hostname", "", ""},
+			cli.StringFlag{"target-resolver", "", ""},
+			cli.StringFlag{"metadata", "", ""},
+			cli.IntFlag{"timeout", -1, ""},
+			cli.IntFlag{"period", -1, ""},
 		},
 	},
 }
